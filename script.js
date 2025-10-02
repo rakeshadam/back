d7e8b28de3c3a885dd4ac781877ede9136597a35
rakeshadam <rakeshadams7@gmail.com>
2025-10-02 15:18:34 +0530

    calc

---
+++ script.js
@@ -0,0 +1,30 @@
+let display = document.getElementById("display");
+
+function appendNumber(number) {
+  display.value += number;
+}
+
+function appendOperator(operator) {
+  if (display.value === "") return;
+  let lastChar = display.value.slice(-1);
+  if ("+-*/".includes(lastChar)) {
+    display.value = display.value.slice(0, -1);
+  }
+  display.value += operator;
+}
+
+function clearDisplay() {
+  display.value = "";
+}
+
+function deleteLast() {
+  display.value = display.value.slice(0, -1);
+}
+
+function calculate() {
+  try {
+    display.value = eval(display.value);
+  } catch {
+    display.value = "Error";
+  }
+}
